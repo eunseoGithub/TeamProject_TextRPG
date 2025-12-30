ab407ab785a1fb84a3bb9956b07db4ca5c31e883
zzohyeyul <zzohyeyul@gmail.com>
2025-12-30 22:51:44 +0900

    add
    Item Class 작성

--- TeamProject_TextRPG/TeamProject_TextRPG/AttackBoost.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/AttackBoost.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "Item.h"
+
 class AttackBoost : public Item
 {
+private:
+	int bonusAttack;
+
+public:
+	AttackBoost(int bonus = 10);
+
+	std::string GetName() const override;
+	void Use(Character& character) override;
 };
 
