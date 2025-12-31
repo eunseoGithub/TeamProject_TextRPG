951d4ba6bca4edeb1b3cbe229ec042319892c339
IMGROOT32 <161199439+IMGROOT32@users.noreply.github.com>
2025-12-31 09:41:23 +0900

    Add : Character class added (#2)

--- TeamProject_TextRPG/TeamProject_TextRPG/Character.cpp
+++ TeamProject_TextRPG/TeamProject_TextRPG/Character.cpp
@@ -1,1 +1,113 @@
 #include "Character.h"
+
+Character::Character(string name)
+{
+	this->name = name;
+	level = 1;
+	hp = 100;
+	attack = 10;
+}
+
+void Character::PrintCharacterStatus()
+{
+	cout << "이름 : " << name << "레벨 : " << level << "체력 : " << hp << "공격력 : " << attack << endl;
+}
+
+string Character::GetName()
+{
+	return name;
+}
+int Character::GetLevel()
+{
+	return level;
+}
+int Character::GetHp()
+{
+	return hp;
+}
+int Character::GetMaxHp()
+{
+	return maxhp;
+}
+int Character::GetAttack()
+{
+	return attack;
+}
+int Character::GetExperience()
+{
+	return experience;
+}
+int Character::GetGold()
+{
+	return gold;
+}
+int Character::GetInventory()
+{
+	return inventory;
+}
+bool Character::GetIsAlive()
+{
+	return isAlive;
+}
+
+
+void Character::SetName(string name)
+{
+	this->name = name;
+}
+void Character::SetHp(int hp)
+{
+	this->hp = hp;
+}
+void Character::SetMaxHp(int maxhp)
+{
+	this->maxhp = maxhp;
+}
+void Character::SetAttack(int attack)
+{
+	this->attack = attack;
+}
+void Character::SetExperience(int experience)
+{
+	this->experience = experience;
+}
+void Character::SetGold(int gold)
+{
+	this->gold = gold;
+}
+void Character::SetInventory(int inventory)
+{
+	this->inventory = inventory;
+}
+void Character::TakeDamage(int damage)
+{
+	this->damage = damage;
+
+	hp = hp - damage;
+	if (hp <= 0)
+	{
+		isAlive = false;
+	}
+	cout << name << "데미지를 입었습니다." << "남은 hp :" << hp << endl;
+}
+
+void Character::Attack(Monster& monster)
+{
+	//monster.TakeDamage
+    // (attack)
+}
+
+void Character::LevelUp()
+{
+	level += 1;
+}
+
+void Character::Dead()
+{
+	cout << name << "이 죽었습니다" << endl;
+}
+
+void Character::SetIsAlive(bool isAlive)
+{
+	this->isAlive = isAlive;
+}
--- TeamProject_TextRPG/TeamProject_TextRPG/Character.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/Character.h
@@ -1,7 +1,49 @@
 #pragma once
 #include "Global.h"
-
+#include "Monster.h"
 class Character
 {
+public:
+	Character(string name);
+
+	void PrintCharacterStatus();
+	void LevelUp();
+
+	string  GetName();
+	int GetLevel();
+	int GetHp();
+	int GetMaxHp();
+	int GetAttack();
+	int GetExperience();
+	int GetGold();
+	int GetInventory();
+	bool GetIsAlive();
+
+	void SetName(string name);
+	void SetHp(int hp);
+	void SetMaxHp(int maxhp);
+	void SetAttack(int attack);
+	void SetExperience(int experience);
+	void SetGold(int gold);
+	void SetInventory(int inventory);
+	void Attack(Monster& monster);
+	void TakeDamage(int damage);
+	void SetIsAlive(bool isAlive);
+
+	void Dead();
+
+private:
+
+	string name;
+	int level;
+	int hp;
+	int maxhp;
+	int attack;
+	int experience;
+	int gold;
+	int inventory;
+	int damage;
+	int dead;
+	bool isAlive;
 };
 
