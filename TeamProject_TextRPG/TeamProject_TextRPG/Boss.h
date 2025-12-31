73051845807480de801cfebbfdfd90aad26f9190
kkym323 <kkym32387@gmail.com>
2025-12-31 12:12:21 +0900

    Add : Boss class added

--- TeamProject_TextRPG/TeamProject_TextRPG/Boss.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/Boss.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "Monster.h"
+
+class Boss : public Monster
+{
+public:
+	Boss(int level);
+	string GetName() override;
+	int GetHealth() override;
+	int GetAttack() override;
+	void TakeDamage(int damage) override;
+	void Attack(Character& character) override;
+	void Dead() override;
+	bool GetIsAlive() override;
+	void SetIsAlive(bool alive) override;
+private:
+	string name;
+	int health;
+	int attack;
+	bool isAlive;
+};
