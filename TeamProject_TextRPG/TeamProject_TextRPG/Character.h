131af7386442be3b7525ff1799990b54cf4474db
IMGROOT32 <161199439+IMGROOT32@users.noreply.github.com>
2025-12-31 16:48:33 +0900

    Update : Character class (#7)
    -inventory() add
    -item() add
    -level() add

--- TeamProject_TextRPG/TeamProject_TextRPG/Character.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/Character.h
@@ -1,14 +1,38 @@
 #pragma once
 #include "Global.h"
 #include "Monster.h"
+#include "Item.h"
+#include <vector>
 class Character
 {
 public:
 	Character(string name);
 
+	//상태 출력
 	void PrintCharacterStatus();
-	void LevelUp();
 
+	//전투
+	void Attack(Monster& monster);
+	void TakeDamage(int damage);
+	void Dead();
+
+	//경험치 / 레벨
+	void LevelUp();
+	void CheckLevelUp();
+	void AddExperience(int amount);
+
+	//아이템 효과
+	void Heal(int amount);
+	void AddTempAttack(int amount);
+	void ResetTempAttack(int amount);
+
+	void DrinkPotion();
+
+	//인벤토리
+	void AddItem(Item* item);
+	vector<Item*>& GetInventory();
+
+	//getter
 	string  GetName();
 	int GetLevel();
 	int GetHp();
@@ -16,34 +40,32 @@ public:
 	int GetAttack();
 	int GetExperience();
 	int GetGold();
-	int GetInventory();
 	bool GetIsAlive();
 
+	//setter
 	void SetName(string name);
 	void SetHp(int hp);
 	void SetMaxHp(int maxhp);
 	void SetAttack(int attack);
 	void SetExperience(int experience);
 	void SetGold(int gold);
-	void SetInventory(int inventory);
-	void Attack(Monster& monster);
-	void TakeDamage(int damage);
 	void SetIsAlive(bool isAlive);
-
-	void Dead();
 
 private:
 
 	string name;
+
 	int level;
 	int hp;
 	int maxhp;
+
 	int attack;
+
 	int experience;
 	int gold;
-	int inventory;
-	int damage;
-	int dead;
+	int item;
+	
+	vector<Item*> inventory;
 	bool isAlive;
 };
 
