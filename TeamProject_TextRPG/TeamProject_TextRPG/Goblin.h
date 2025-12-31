45ceac38206e069841e46395860db6b475b9691a
kkym323 <kkym32387@gmail.com>
2025-12-31 09:42:14 +0900

    add : Monster class added (#1)

--- TeamProject_TextRPG/TeamProject_TextRPG/Goblin.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/Goblin.h
@@ -1,6 +1,22 @@
 #pragma once
 #include "Monster.h"
+
 class Goblin : public Monster
 {
+public:
+	Goblin(int level);
+	string GetName() override;
+	int GetHealth() override;
+	int GetAttack() override;
+	void TakeDamage(int damage) override;
+	void Attack(Character& character) override;
+	void Dead() override;
+	bool GetIsAlive() override;
+	void SetIsAlive(bool alive) override;
+private:
+	string name;
+	int health;
+	int attack;
+	bool isAlive;
 };
 
--- TeamProject_TextRPG/TeamProject_TextRPG/Monster.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/Monster.h
@@ -1,7 +1,17 @@
 #pragma once
 #include"Global.h"
+#include "Character.h"
 
 class Monster
 {
+public:
+	virtual string GetName() = 0;
+	virtual int GetHealth() = 0;
+	virtual int GetAttack() = 0;
+	virtual void TakeDamage(int damage) = 0;
+	virtual void Attack(Character& character) = 0;
+	virtual void Dead() = 0;
+	virtual bool GetIsAlive() = 0;
+	virtual void SetIsAlive(bool alive) = 0;
 };
 
