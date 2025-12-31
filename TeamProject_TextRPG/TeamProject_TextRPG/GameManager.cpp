aecba1df6549f3f467bf670b99e93fa4ac539f40
eunseoGithub <rhkrdmstj01@naver.com>
2025-12-31 09:38:34 +0900

    Add : GameManager added

--- TeamProject_TextRPG/TeamProject_TextRPG/GameManager.cpp
+++ TeamProject_TextRPG/TeamProject_TextRPG/GameManager.cpp
@@ -1,1 +1,21 @@
 #include "GameManager.h"
+
+GameManager::GameManager()
+{
+	
+}
+
+Monster* GameManager::GenerateMonster(int level)
+{
+	return new Monster();
+}
+
+void GameManager::Battle(Character* character)
+{
+	
+}
+
+const void GameManager::DisplayInventory(const Character* const character)
+{
+
+}
--- TeamProject_TextRPG/TeamProject_TextRPG/GameManager.h
+++ TeamProject_TextRPG/TeamProject_TextRPG/GameManager.h
@@ -1,5 +1,15 @@
 #pragma once
+#include "Monster.h"
+#include "Character.h"
+
 class GameManager
 {
+public:
+	GameManager();
+	Monster* GenerateMonster(int level);
+	void Battle(Character* character);
+	const void DisplayInventory(const Character* const character);
+private:
+	
 };
 
